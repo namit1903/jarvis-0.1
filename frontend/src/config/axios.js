3bce68aaea314bc6555108e3f84489f3b3368674
Thakur <namit1903@gmail.com>
2025-01-17 15:35:22 +0530

    frontend and controllers + services

---
+++ frontend/src/config/axios.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import 'dotenv/config'
+//so as to prepend the baseURL to all requests, avoiding repetitive code
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        "Authorization": `Bearer ${localStorage.getItem('token')}`// If you store the token in the Authorization header and not in a cookie, you can avoid storing the token in cookies altogether
+        //This means you don't have to worry about cookie-based attacks like Cross-Site Request Forgery (CSRF), since cookies are not being automatically sent with every request.
+    }
+})
+
+
+export default axiosInstance;   
