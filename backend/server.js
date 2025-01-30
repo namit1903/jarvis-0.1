70a9ed1401b7b9fef4ce55ef7782ce3e15189f47
Thakur <namit1903@gmail.com>
2025-01-30 15:41:44 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);//created http server
 const io = new Server(server, {//http server instance is passed as argument to 
     cors: {
-        origin: 'https://jarvis-0-1-3ewj.vercel.app/login'
+        origin: 'https://jarvis-0-1-3ewj.vercel.app/login',
+        methods: ["GET", "POST"],
+        credentials: true
     }
 });
 //io is an instance of socket.io which will be used to manage websocket connections.
