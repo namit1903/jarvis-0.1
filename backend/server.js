1c845c6d11b020effb3283a6a3ab15c49d627a76
Thakur <namit1903@gmail.com>
2025-01-30 15:42:43 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);//created http server
 const io = new Server(server, {//http server instance is passed as argument to 
     cors: {
-        origin: 'https://jarvis-0-1-3ewj.vercel.app/login',
+        origin: 'https://jarvis-0-1-3ewj.vercel.app/',
         methods: ["GET", "POST"],
         credentials: true
     }
